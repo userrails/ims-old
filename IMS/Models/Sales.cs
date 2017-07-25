28eba394efb764e41109454e48fb6fe8acebbd8f
userrails <userrails@gmail.com>
2017-07-25 13:57:09 +0545

    Sales model created

---
+++ IMS/Models/Sales.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMS.Models
+{
+    public class Sales
+    {
+        public int Id { get; set; }
+        public int VendorId { get; set; }
+        public virtual Vendor Vendor { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int Qty { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public bool IsVatTaken { get; set; }
+    }
+}
