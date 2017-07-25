4dbb45e79a1c1a19b82aa1b98e68d936eb405c77
userrails <userrails@gmail.com>
2017-07-25 15:25:26 +0545

    stock class added

---
+++ IMS/Models/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMS.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int Qty { get; set; }
+    }
+}
