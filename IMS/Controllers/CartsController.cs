6fd7b7e619d3622925914e2fbad0ad1b0df1a849
userrails <userrails@gmail.com>
2017-08-03 14:10:06 +0545

    carts form vendors create successful

---
+++ IMS/Controllers/CartsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IMS.Models;
+
+namespace IMS.Controllers
+{
+    public class CartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Carts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id, Name, Address, Panno, Remarks")] Vendor vendor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vendors.Add(vendor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(vendor);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // uniqueness validation code
+        public JsonResult IsNameExists(string Name)
+        {
+            if (db.Vendors != null)
+            {
+                return Json(!db.Vendors.Any(n => n.Name == Name), JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        // uniqueness validation code
+        public JsonResult IsPanExists(string Panno)
+        {
+            if (db.Vendors != null)
+            {
+                return Json(!db.Vendors.Any(n => n.Panno == Panno), JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(null, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
